96877e09c89d6c14745da628616057a111e35ccc
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-01-07 16:29:52 +0400

    Implement turns

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use tetra::graphics::{self, Color, Texture};
+use tetra::graphics::DrawParams;
+use tetra::graphics::mesh::Mesh;
+use tetra::graphics::{self, Color, Texture, Rectangle, mesh::{ShapeStyle}};
+use tetra::input::{self, MouseButton};
 use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
@@ -7,23 +10,48 @@ const TITLE: &str = "Tic Tac Toe";
 const SCREEN_WIDTH: f32 = 800.0;
 const SCREEN_HEIGHT: f32 = 600.0;
 
+struct GameState {
+    current_turn: Turn,
+    grid: [CellState; 9],
+    textures: MarkTextures,
+}
+
+#[derive(Clone, Copy)]
+enum Turn {
+    X,
+    O,
+}
+
+#[derive(Clone, Copy)]
+enum CellState {
+    Empty,
+    X,
+    O,
+}
+
+impl From<Turn> for CellState {
+    fn from(turn: Turn) -> Self {
+        match turn {
+            Turn::O => CellState::O,
+            Turn::X => CellState::X,
+        }
+    }
+}
+
 struct MarkTextures {
     x: Texture,
     o: Texture,
 }
-struct GameState {
-    textures: MarkTextures,
-}
 
 impl GameState {
     fn new(ctx: &mut Context) -> tetra::Result<GameState> {
-        let textures = MarkTextures {
-            x: Texture::new(ctx, "resources/x.png")?,
-            o: Texture::new(ctx, "resources/o.png")?,
-        };
-
         Ok(GameState {
-            textures
+            current_turn: Turn::X,
+            grid: [CellState::Empty; 9],
+            textures: MarkTextures {
+                x: Texture::new(ctx, "resources/x.png")?,
+                o: Texture::new(ctx, "resources/o.png")?,
+            },
         })
     }
 }
@@ -32,14 +60,82 @@ impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
 
-        let total_width: f32 = 64.0 * 2.0 + 32.0;
-        let total_height: f32 = 64.0;
+        let mark_size = self.textures.o.width() as f32;
+        let gap_size: f32 = 15.0;
+        let bbox_side: f32 = mark_size * 3.0 + gap_size * 2.0;
 
-        let start_x = (SCREEN_WIDTH - total_width) / 2.0;
-        let start_y = (SCREEN_HEIGHT - total_height) / 2.0;
+        let start_x = (SCREEN_WIDTH - bbox_side) / 2.0;
+        let start_y = (SCREEN_HEIGHT - bbox_side) / 2.0;
 
-        self.textures.x.draw(ctx, Vec2::new(start_x, start_y));
-        self.textures.o.draw(ctx, Vec2::new(start_x + 64.0 + 32.0, start_y));
+        Mesh::rectangle(ctx, ShapeStyle::Stroke(7.0), Rectangle::new(
+            start_x - 15.0,
+            start_y - 15.0,
+            bbox_side + 30.0,
+            bbox_side + 30.0,
+        ))?.draw(ctx, DrawParams::new());
+
+        for (i, &item) in self.grid.iter().enumerate() {
+            let x = i % 3;
+            let y = i / 3;
+
+            if let CellState::Empty = item {
+                continue;
+            }
+            let texture = match item {
+                CellState::X => &self.textures.x,
+                CellState::O => &self.textures.o,
+                CellState::Empty => unreachable!(),
+            };
+
+            texture.draw(
+                ctx,
+                Vec2::new(
+                    start_x + x as f32 * (mark_size + gap_size),
+                    start_y + y as f32 * (mark_size + gap_size),
+                ),
+            )
+        }
+
+        Ok(())
+    }
+
+    fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        if input::is_mouse_button_down(ctx, MouseButton::Left) {
+            let mouse_pos = input::get_mouse_position(ctx);
+
+            let mark_size = self.textures.o.width() as f32;
+            let gap_size: f32 = 15.0;
+            let bbox_side: f32 = mark_size * 3.0 + gap_size * 2.0;
+
+            let start_x = (SCREEN_WIDTH - bbox_side) / 2.0;
+            let start_y = (SCREEN_HEIGHT - bbox_side) / 2.0;
+
+            let end_x = start_x + bbox_side;
+            let end_y = start_y + bbox_side;
+
+            if mouse_pos.x >= start_x
+                && mouse_pos.x <= end_x
+                && mouse_pos.y >= start_y
+                && mouse_pos.y <= end_y
+            {
+                let offset_x = mouse_pos.x - start_x;
+                let offset_y = mouse_pos.y - start_y;
+
+                let cell_side = bbox_side / 3.0;
+
+                let ix = (offset_x / cell_side) as usize;
+                let iy = (offset_y / cell_side) as usize;
+
+                let ref mut cell = self.grid[iy * 3 + ix];
+                if let CellState::Empty = cell {
+                    *cell = self.current_turn.into();
+                    self.current_turn = match self.current_turn {
+                        Turn::O => Turn::X,
+                        Turn::X => Turn::O,
+                    }
+                }
+            }
+        }
 
         Ok(())
     }
