66d26f08647187f14dee979b6bdef2f22e84f5f4
NoneTheWisr <12655815+NoneTheWisr@users.noreply.github.com>
2022-01-07 14:57:14 +0400

    Add marks

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
-use tetra::graphics::{self, Color};
+use tetra::graphics::{self, Color, Texture, DrawParams};
+use tetra::math::Vec2;
 use tetra::{Context, ContextBuilder, State};
 
-struct GameState;
+const TITLE: &str = "Tic Tac Toe";
+
+const SCREEN_WIDTH: f32 = 800.0;
+const SCREEN_HEIGHT: f32 = 600.0;
+
+struct MarkTextures {
+    x: Texture,
+    o: Texture,
+}
+struct GameState {
+    textures: MarkTextures,
+}
+
+impl GameState {
+    fn new(ctx: &mut Context) -> tetra::Result<GameState> {
+        let textures = MarkTextures {
+            x: Texture::new(ctx, "resources/x.png")?,
+            o: Texture::new(ctx, "resources/o.png")?,
+        };
+
+        Ok(GameState {
+            textures
+        })
+    }
+}
 
 impl State for GameState {
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
-        // Cornflower blue, as is tradition
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        let total_width: f32 = 64.0 * 2.0 + 32.0;
+        let total_height: f32 = 64.0;
+
+        let start_x = (SCREEN_WIDTH - total_width) / 2.0;
+        let start_y = (SCREEN_HEIGHT - total_height) / 2.0;
+
+        self.textures.x.draw(ctx, Vec2::new(start_x, start_y));
+        self.textures.o.draw(ctx, Vec2::new(start_x + 64.0 + 32.0, start_y));
+
         Ok(())
     }
 }
 
 fn main() -> tetra::Result {
-    ContextBuilder::new("Hello, world!", 1280, 720)
+    ContextBuilder::new(TITLE, SCREEN_WIDTH as i32, SCREEN_HEIGHT as i32)
+        .quit_on_escape(true)
+        .show_mouse(true)
         .build()?
-        .run(|_| Ok(GameState))
+        .run(GameState::new)
 }
